8d5ecbad0432e1f4c712370ffd7021d83a3564c3
Risham Soni <rishamsoni.tech@gmail.com>
2026-07-28 20:32:26 +0530

    WEEK 1

--- src/Main.java
+++ src/Main.java
@@ -139,3 +139,29 @@ class Main{
     }
 }
 */
+
+//Find Permutation
+/*
+class Main{
+    public static void FindPermutation(String str, String ans){
+        //base case
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        //recursion
+        for(int i = 0; i < str.length(); i++){
+            char curr = str.charAt(i);
+            String NewStr = str.substring(0, i) + str.substring(i+1);
+            FindPermutation(NewStr, ans+curr);
+        }
+    }
+
+    public static void main(String args[]){
+        String str = "abc";
+        FindPermutation(str, " ");
+    }
+}
+*/
+
