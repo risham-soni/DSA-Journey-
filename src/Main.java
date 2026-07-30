454b4ef40e61d518431fa1180b861d8d42ec0a65
Risham Soni <rishamsoni.tech@gmail.com>
2026-07-30 21:51:55 +0530

    WEEK 1

--- src/Main.java
+++ src/Main.java
@@ -250,3 +250,37 @@ class Main{
     }
 }
 */
+
+
+
+//Removing Stars From a String
+/*
+import java.util.Stack;
+
+class Main {
+    public static String removes(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '*') {
+                if (!st.isEmpty()) {
+                    st.pop();
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : st) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+
+    public static void main(String args[]) {
+        String s = "le*tco*e";
+        System.out.println(removes(s));
+    }
+}
+*/
