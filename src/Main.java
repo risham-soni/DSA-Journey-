e0c31339c30ce1706176985f33192bd403c647b4
Risham Soni <rishamsoni.tech@gmail.com>
2026-08-01 16:19:44 +0530

    WEEK 1

--- src/Main.java
+++ src/Main.java
@@ -398,9 +398,8 @@ class Main{
 
 
 // DP -> Climbing Stairs
-
+/*
 import java.util.Arrays;
-
 class Main{
     public static int Solve(int n, int ways[]){
         //base case
@@ -420,10 +419,93 @@ class Main{
         return ways[n];
     }
     public static void main(String args[]){
-        int n = 3;
+        int n = 5;
         int ways[] = new int[n + 1];
         Arrays.fill(ways, -1);
         System.out.println(Solve(n, ways));
     }
 }
+*/
+
+
+//Rotten Orange
+/*
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Main {
+
+    public static int Solve(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        int fresh = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        if (fresh == 0) {
+            return 0;
+        }
+
+        int minutes = 0;
+
+        int[][] direction = {
+                {1, 0},
+                {-1, 0},
+                {0, 1},
+                {0, -1}
+        };
+
+        while (!queue.isEmpty() && fresh > 0) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] current = queue.poll();
+
+                int x = current[0];
+                int y = current[1];
+
+                for (int[] dir : direction) {
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+
+                    if (nx >= 0 && nx < row &&
+                            ny >= 0 && ny < col &&
+                            grid[nx][ny] == 1) {
+
+                        grid[nx][ny] = 2;
+                        fresh--;
+                        queue.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+
+            minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {
+                {2, 1, 1},
+                {1, 1, 0},
+                {0, 1, 1}
+        };
+
+        System.out.println(Solve(grid));
+    }
+}
+*/
+
 
