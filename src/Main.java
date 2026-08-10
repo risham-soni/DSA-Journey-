886061f499b45fd130b4efc13f5d9974c7353b27
Risham Soni <rishamsoni.tech@gmail.com>
2026-08-10 17:38:35 +0530

    WEEK 2

--- src/Main.java
+++ src/Main.java
@@ -620,3 +620,86 @@ class Main {
     }
 }
 */
+
+
+//01 knapsack using memoization
+/*
+class Main{
+    public static int knapsack(int val[], int[] wt, int w, int n, int dp[][]){
+        if(w == 0 || n == 0){
+            return 0;
+        }
+
+        if(dp[n][w] != -1){
+            return dp[n][w];
+        }
+
+        if(wt[n-1] <= w){ //valid
+            //include
+            int ans1 = val[n-1]+knapsack(val, wt, w-wt[n-1], n-1, dp);
+
+            //exclude
+            int ans2 = knapsack(val, wt, w, n-1, dp);
+            dp [n][w] = Math.max(ans1, ans2);
+            return dp[n][w];
+        }else {
+            dp[n][w] = knapsack(val, wt, w, n-1, dp);
+            return dp[n][w];
+        }
+    }
+    public static void main(String args[]){
+        int val[] = {15, 14, 10, 45, 30};
+        int wt[] = {2, 5, 1, 3, 4};
+        int w = 7;
+        int dp[][] = new int[val.length+1][w+1];
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(knapsack(val, wt, w, val.length, dp));
+    }
+}
+*/
+
+//Coin change problem
+/*
+public class Main{
+    public static int coinsPossibleCombinations(int[] coins, int amount){
+        int dp[] = new int[amount + 1];
+        //Base Case
+
+        dp[0] = 1;
+
+        for(int coin : coins){
+            for(int i = coin; i <= amount; i++){
+                dp[i] = dp[i] + dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+    public static void main(String args[]){
+        int coins[] = {1, 2, 5};
+        int amount = 5;
+        System.out.println(coinsPossibleCombinations(coins, amount));
+    }
+}
+*/
+
+//count the no if integer
+/*
+class Main{
+    public static void main(String args[]){
+        int n = 716501;
+        int count = 0;
+        while(n != 0) {
+            int digit = n % 10;
+            n = n / 10;
+            count++;
+        }
+        System.out.print(count);
+    }
+}
+*/
+
