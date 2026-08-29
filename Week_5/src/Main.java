f0cf3fab64f0601e4fad8d6accd552dea030a420
Risham Soni <rishamsoni.tech@gmail.com>
2026-08-29 21:53:09 +0530

    WEEK 5

--- Week_5/src/Main.java
+++ Week_5/src/Main.java
@@ -92,4 +92,15 @@ class Main{
 }
 */
 
-//
+//Basic Patterns
+class Main{
+    public static void main(String args[]){
+        int n = 4;
+        for(int i = 0; i <= n; i++){
+            for(int s = 1; s <= i; s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
